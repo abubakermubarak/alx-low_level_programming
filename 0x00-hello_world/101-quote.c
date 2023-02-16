1daec0dd40ae412add28cb4a1120722b0431c0de
abubakermubarak <abubakermubarak95@gmail.com>
2023-02-16 14:47:59 -0800

    print a string with fprintf and stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19");
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", msg);
 	return (1);
 }
