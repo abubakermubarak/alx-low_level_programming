345e7e48fb00bdd62ba22a3c796c6558a48e9a5c
abubakermubarak <abubakermubarak95@gmail.com>
2023-02-16 11:27:29 -0800

    Write a C program that prints using puts instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int main(void) 
 {
 /**
@@ -8,6 +7,6 @@ int main(void)
  * Return: Always 0 (Success)
  */
 	puts("\"Programming is like building a multilingual puzzle");
-		return 0;
+		return (0);
 }
 
