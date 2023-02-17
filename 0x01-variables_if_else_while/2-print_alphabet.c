a3b7e5b732dbc28d11d9e5144808af391bc23bca
abubakermubarak <abubakermubarak95@gmail.com>
2023-02-17 14:03:23 -0800

    program print lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alway return 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	return (0);
+}
+
